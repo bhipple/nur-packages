3124a86936eb0137584876fc744315a686219f30
Benjamin Hipple <bhipple@protonmail.com>
2020-03-28 19:24:31 -0400

    upgrade emacs overlay

--- overlays/emacs-overlay.nix
+++ overlays/emacs-overlay.nix
@@ -1,6 +1,6 @@
 let
-  rev = "fa451069340a5fdac9b6497ce78989d4b73114a4";
-  sha256 = "19v6w6bmw7mjbpnyjjhjw0lx5icz3d8llfpnb5118g7f9p5005cc";
+  rev = "0f0f5d2bb1355507630f58f9db4bc6897ee12687";
+  sha256 = "1a5cg0b86vp41iadzybn5wdqjlzzixamig71wmhry3grdamqj6i6";
   emacs-overlay = import (builtins.fetchTarball {
     url = "https://github.com/nix-community/emacs-overlay/archive/${rev}.tar.gz";
     inherit sha256;
