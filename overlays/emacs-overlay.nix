cc955b901ca34ce9d97d6b285cb7083803228390
Benjamin Hipple <bhipple@protonmail.com>
2020-05-21 11:12:40 -0400

    Bump emacs overlay

--- overlays/emacs-overlay.nix
+++ overlays/emacs-overlay.nix
@@ -1,6 +1,6 @@
 let
-  rev = "64bda20d1a0d062e159bd0a8d398d0741af83375";
-  sha256 = "1z5dyalv56fja0xcqqksccyybx0b4a9gh0wk8mk36l7kkdz3hmbp";
+  rev = "7107b459905f3fad1d27519fc7babed6e7e39491";
+  sha256 = "12zmfycl8nv55qmdrgfn1k26iwiiq6rh5q8427dg6v3qfk58gqnb";
   emacs-overlay = import (
     builtins.fetchTarball {
       url = "https://github.com/nix-community/emacs-overlay/archive/${rev}.tar.gz";
