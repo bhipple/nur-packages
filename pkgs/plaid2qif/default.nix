c4d3a4c58fae09533bf5b1c9d41678b5429d3900
Benjamin Hipple <bhipple@protonmail.com>
2020-12-26 14:56:24 -0500

    WIP on plaid2qif

--- pkgs/plaid2qif/default.nix
+++ pkgs/plaid2qif/default.nix
@@ -4,13 +4,15 @@ python3Packages.buildPythonApplication rec {
   pname = "plaid2qif";
   version = "1.3.2";
 
-  src = python3Packages.fetchPypi {
-    inherit pname version;
-    sha256 = "0h92g397y9d7qb5hi6gdnhq077q80v0kkcicbyglal4fiihyp2c4";
-  };
+  # src = python3Packages.fetchPypi {
+  #   inherit pname version;
+  #   sha256 = "0h92g397y9d7qb5hi6gdnhq077q80v0kkcicbyglal4fiihyp2c4";
+  # };
+  src = /home/bhipple/src/plaid2qif;
 
   # TODO: See if we can fix setup.py instead.
   patchPhase = ''
+    rm -f requirements.txt
     for dep in plaid-python wheel twine docopt python-dateutil; do
       echo $dep >> requirements.txt
     done
