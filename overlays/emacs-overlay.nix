e530ac116f32ace480cf268f0fe46f4e3f079f41
Benjamin Hipple <bhipple@protonmail.com>
2022-11-06 09:58:13 -0500

    Manage spacemacs pkgs with package.el
    It's a lot less robust, but a lot simpler. Now that I'm using VSCode primarily
    and just using emacs for org-mode this tradeoff makes more sense.

--- overlays/emacs-overlay.nix
+++ overlays/emacs-overlay.nix
@@ -1,6 +1,6 @@
 let
-  rev = "70787f42dbf53b8a57c4b9181fd98da1d710ac5b"; # updated 2022-10-02
-  sha256 = "03nqdk4j0grbjchqaxrxx5mxgvzrfg4wryw4iq6xzqmxliyk3liq";
+  rev = "c009b388c8c2514b24baf6231c5612192c25745c";  # updated 2022-11-06
+  sha256 = "007nnjwdyxxa0kq12ixskxy1l312c03fwzylgiczbsnpyb2hky8i";
   emacs-overlay = import (
     builtins.fetchTarball {
       url = "https://github.com/nix-community/emacs-overlay/archive/${rev}.tar.gz";
