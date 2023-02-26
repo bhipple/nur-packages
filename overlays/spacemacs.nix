37cff4510e8509d870d2aaaa3e96fc4ac327f0e3
Benjamin Hipple <bhipple@protonmail.com>
2023-02-26 18:02:55 -0500

    Native comp is now default in emacs 28+

--- overlays/spacemacs.nix
+++ overlays/spacemacs.nix
@@ -24,7 +24,7 @@ let
   # Build a spacemacs with the pinned overlay import
   spacemacs = self.emacsWithPackagesFromUsePackage {
     config = "";
-    package = self.emacsPgtkNativeComp;
+    package = self.emacsUnstable;
     extraEmacsPackages = _: myEmacsDeps;
   };
 
