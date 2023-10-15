5bc7773958615b1b90886eb04d10312850ae4e53
Benjamin Hipple <bhipple@protonmail.com>
2023-10-15 09:43:59 -0400

    emacs

--- overlays/emacs-overlay.nix
+++ overlays/emacs-overlay.nix
@@ -1,6 +1,6 @@
 let
-  rev = "81465f07ee68381dc370f9da1a882d581c335338"; # updated 2023-08-25
-  sha256 = "19a9q7b9qinqklhna3cnrlhx4ml42c4ybwcmyhb8q1h4rvil6zln";
+  rev = "5a93a5dad00a9028b8e54a40208f878c4aaf5a1c"; # updated 2023-10-15
+  sha256 = "0z8gz41klsmdz2zk7fsm7z20wvh87m4r1wgsn3qdbs9jikyabxxv";
   emacs-overlay = import (
     builtins.fetchTarball {
       url = "https://github.com/nix-community/emacs-overlay/archive/${rev}.tar.gz";
